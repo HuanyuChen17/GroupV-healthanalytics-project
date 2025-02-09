2de16b161ada39c7aafa97e56fa6697e74c5e8eb
1noehftb <yw6624@ic.ac.uk>
2025-02-09 16:04:13 +0000

    Update Group V.R

--- Group V.R
+++ Group V.R
@@ -135,36 +135,6 @@ ggplot(data_summary, aes(x = EDUC_GROUP, y = HPV_Rate, fill = EDUC_GROUP)) +
   scale_fill_brewer(palette = "Set2")
 
 
-
-
-# Visualization: Extract coefficients and standard errors for education levels
-data_multilevel <- heterogeneity_results %>%
-  filter(Variable %in% c("EDUC_GROUPHigh School Graduate", 
-                         "EDUC_GROUPSome College or Bachelor", 
-                         "EDUC_GROUPHigher")) %>%
-  mutate(
-    Education = case_when(
-      Variable == "EDUC_GROUPHigh School Graduate" ~ "High School Graduate",
-      Variable == "EDUC_GROUPSome College or Bachelor" ~ "Some College or Bachelor",
-      Variable == "EDUC_GROUPHigher" ~ "Higher Education"
-    )
-  ) %>%
-  select(Region, Education, Coefficient = Estimate, SE = `Std. Error`)
-
-# Plot subfigures
-ggplot(data_multilevel, aes(x = Region, y = Coefficient, fill = Education)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  geom_errorbar(aes(ymin = Coefficient - 1.96 * SE, ymax = Coefficient + 1.96 * SE), 
-                position = position_dodge(0.9), width = 0.2) +
-  facet_wrap(~ Education, ncol = 1) +
-  labs(title = "Regional Differences in HPV Vaccination by Education Level",
-       x = "Region", y = "Coefficient") +
-  theme_minimal() +
-  scale_fill_brewer(palette = "Set2")
-
-
-
-
 # Main Regression
 # Function to format p-values exactly as displayed in R terminal
 format_pvalue <- function(pval) {
@@ -214,6 +184,32 @@ vif_results <- vif(lpm_model)
 # Print VIF results 
 print("Variance Inflation Factor (VIF) Results:") 
 print(vif_results)
+
+
+# Visualization: Extract coefficients and standard errors for education levels
+data_multilevel <- heterogeneity_results %>%
+  filter(Variable %in% c("EDUC_GROUPHigh School Graduate", 
+                         "EDUC_GROUPSome College or Bachelor", 
+                         "EDUC_GROUPHigher")) %>%
+  mutate(
+    Education = case_when(
+      Variable == "EDUC_GROUPHigh School Graduate" ~ "High School Graduate",
+      Variable == "EDUC_GROUPSome College or Bachelor" ~ "Some College or Bachelor",
+      Variable == "EDUC_GROUPHigher" ~ "Higher Education"
+    )
+  ) %>%
+  select(Region, Education, Coefficient = Estimate, SE = `Std. Error`)
+
+# Plot subfigures
+ggplot(data_multilevel, aes(x = Region, y = Coefficient, fill = Education)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_errorbar(aes(ymin = Coefficient - 1.96 * SE, ymax = Coefficient + 1.96 * SE), 
+                position = position_dodge(0.9), width = 0.2) +
+  facet_wrap(~ Education, ncol = 1) +
+  labs(title = "Regional Differences in HPV Vaccination by Education Level",
+       x = "Region", y = "Coefficient") +
+  theme_minimal() +
+  scale_fill_brewer(palette = "Set2")
 
 
 # Robustness Check
